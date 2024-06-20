8b923c1620d4182e85c71dadc0b9ea6c7c03d396
Abdalrahman(0x80) <78023331+abdalrahman0x80@users.noreply.github.com>
2024-06-20 14:11:14 +0300

    Add files via upload

---
+++ Base64.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+namespace Base64
+{
+    class EncodingAndDecoding
+    {
+        public string encode(string e)
+        {
+            byte[] encoder = Encoding.UTF8.GetBytes(e);
+            string Converter = Convert.ToBase64String(encoder);
+            return Converter;
+        }
+        public string decode(string e)
+        {
+            var base64 = Convert.FromBase64String(e);
+            string GetString = Encoding.UTF8.GetString(base64);
+            return GetString;
+
+
+        }
+    }
+}
